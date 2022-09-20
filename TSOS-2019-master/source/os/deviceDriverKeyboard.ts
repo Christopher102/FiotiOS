1a2b841dd65e6e503626454d2ea4a8741b01b624
chfioti2201 <christopher.fioti@Nielsen.com>
2022-09-19 23:14:55 -0400

    Added special characters

--- TSOS-2019-master/source/os/deviceDriverKeyboard.ts
+++ TSOS-2019-master/source/os/deviceDriverKeyboard.ts
@@ -31,6 +31,45 @@ module TSOS {
             // Parse the params.  TODO: Check that the params are valid and osTrapError if not.
             var keyCode = params[0];
             var isShifted = params[1];
+            var isCtrl = params[2];
+
+            // Custom objects in JS are kinda nuts. I had no idea you could just straight map values like this, and it makes this so much less complicated. Did not want to write 50+ switch / case statements
+            var shiftedSpecial = { 
+                48: ')',
+                49: '!',
+                50: '@',
+                51: '#',
+                52: '$',
+                53: '%',
+                54: '^',
+                55: '&',
+                56: '*',
+                57: '(',
+                192: '~',
+                173: '_',
+                61: '+',
+                219: '{',
+                221: '}',
+                220: '|',
+                59: ':',
+                222: '"',
+                188: '<',
+                190: '>',
+                191: '?' };
+            
+            var specialChars = { 
+                192: '`',
+                173: '-',
+                61: '=',
+                219: '[',
+                221: ']',
+                220: '\\',
+                59: ';',
+                222: "'",
+                188: ',',
+                190: '.',
+                191: '/' };
+
             _Kernel.krnTrace("Key code:" + keyCode + " shifted:" + isShifted);
             var chr = "";
             // Check to see if we even want to deal with the key that was pressed.
@@ -42,7 +81,19 @@ module TSOS {
                 }
                 // TODO: Check for caps-lock and handle as shifted if so.
                 _KernelInputQueue.enqueue(chr);
-            } else if (((keyCode >= 48) && (keyCode <= 57)) ||   // digits
+            
+            // Handles shifted special characters
+            }else if(keyCode in shiftedSpecial){
+                if(isShifted === true){
+                    _KernelInputQueue.enqueue(shiftedSpecial[keyCode]);
+                }
+            
+            // Handles regular special characters
+            } else if (keyCode in specialChars){
+                _KernelInputQueue.enqueue(specialChars[keyCode]);
+            
+            // Everything else beyond this point
+            }else if (((keyCode >= 48) && (keyCode <= 57))  ||   // digits
                         (keyCode == 32)                     ||   // space
                         (keyCode == 13)                     ||   // enter
                         (keyCode == 8 )                     ||   //Backspace
@@ -50,13 +101,13 @@ module TSOS {
                         ){                      
                 chr = String.fromCharCode(keyCode);
                 _KernelInputQueue.enqueue(chr);
-            } else if(keyCode == 38) {
+            } else if(keyCode == 38 && !isShifted) {
                 chr = "upArrow";
                 _KernelInputQueue.enqueue(chr);
             } else if(keyCode == 40){
                 chr = "downArrow";
                 _KernelInputQueue.enqueue(chr);
             }
-        }
+        } 
     }
 }
